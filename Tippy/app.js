f74708c33a520d9c4283ac27bb2979fe7bc1737f
Najeeb <najeebthangalkp@gmail.com>
2022-10-20 10:04:36 +0530

    Feature : Add a dark mode/light mode toggle

--- Tippy/app.js
+++ Tippy/app.js
@@ -3,6 +3,8 @@ const tipInput = document.getElementById('tipInput');
 const numberofpeople = document.getElementById('numberofpeople');
 const perpersontotal = document.getElementById('perpersontotal');
 const totaltip = document.getElementById('tip');
+const darkMode=document.getElementById('dark-mode');
+const body=document.querySelector("body");
 
 let numberOfPeople = Number(numberofpeople.innerText);
 
@@ -34,6 +36,22 @@ const preventAlphabets = e => {
 
 	if (key >= 65 && key <= 90) e.preventDefault();
 };
+
+const switchDarkMode=()=>{
+	if(darkMode.classList.contains("fa-sun-o")){
+		darkMode.classList.remove("fa-sun-o");
+		darkMode.classList.add("fa-moon-o");
+		darkMode.style.color="white";
+		document.body.style.backgroundImage = "none";
+		document.body.style.background="#000000";
+	}
+else{
+	darkMode.classList.remove("fa-moon-o");
+	darkMode.classList.add("fa-sun-o");
+	darkMode.style.color="black";
+	document.body.style.background="#ffffff";
+	}
+}
 
 billTotalInput.addEventListener('keydown', preventAlphabets);
 tipInput.addEventListener('keydown', preventAlphabets);
